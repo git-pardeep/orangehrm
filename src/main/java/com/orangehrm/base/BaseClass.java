88059a8c423303447df87dc8b9e63b674dbaafed
git-pardeep <pardeepkambojj@gmail.com>
2023-06-10 09:26:40 +0530

    plugin update

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -4,9 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
